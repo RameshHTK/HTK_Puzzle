6b15a3e2f0afdf510eea6a08e4c510a0fb92ff8c
Ramesh Bettge <bettge.r@gmx.de>
2025-01-27 18:11:51 +0100

    ADDED: UI which reacts with highlights and focus to mouse / gamepad input

---
+++ Source/HTK_Puzzle/HTK_Puzzle.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HTK_Puzzle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HTK_Puzzle, "HTK_Puzzle" );
